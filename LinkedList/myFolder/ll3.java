bb4c268096f67ca8d863ef590741140c7eed9add
Vipin Prajapati <vipinpra7348@gmail.com>
2026-03-18 15:42:54 +0530

    Create ll3.java

---
+++ LinkedList/myFolder/ll3.java
@@ -0,0 +1,27 @@
+class Main{
+    static class node{
+        int data;
+        node next;
+        node (int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void displayNode(node head){
+        node temp = head;
+        while(temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+    public static void main(String [] args){
+        node a = new node(1);
+        node b = new node(2);
+        node c = new node(3);
+        a.next = b;
+        b.next = c;
+        //System.out.println(a.next.data);
+        displayNode(a);
+        
+    }
+}
